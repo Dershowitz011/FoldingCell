bdaeb3e2faa16241ee7fb68be2d4166c4ca98f32
Dershowitz <gokuman76@gmail.com>
2016-04-15 16:27:25 +0530

    Initial commit

---
+++ FoldingCell/FoldingCell.h
@@ -0,0 +1,19 @@
+//
+//  FoldingCell.h
+//  FoldingCell
+//
+//  Created by Dershowitz on 15/04/16.
+//  Copyright © 2016 Dershowitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FoldingCell.
+FOUNDATION_EXPORT double FoldingCellVersionNumber;
+
+//! Project version string for FoldingCell.
+FOUNDATION_EXPORT const unsigned char FoldingCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoldingCell/PublicHeader.h>
+
+
